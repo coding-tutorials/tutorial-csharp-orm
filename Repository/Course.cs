1df447bea9c28120bde6907f6c3f593e8ee3bec1
Wagner Leonardi <leonardiwagner@gmail.com>
2013-10-02 21:28:42 -0300

    First commit

---
+++ Repository/Course.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class Course : Repository<Dal.Course,Model.Course>
+    {
+        private Dal.Course dalCourse = new Dal.Course();
+
+        public void x()
+        {
+            var ave = this.dalCourse.Get(1, 3);
+        }
+
+        public List<Model.Course> Get(int skip, int count)
+        {
+            return null;
+        }
+
+        public int Save(Model.Course course)
+        {
+            return 0;
+        }
+
+        protected override Model.Course DalToModel(Dal.Course dal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repository/Repository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public abstract class Repository<DalType,ModelType>
+    {
+
+        protected abstract ModelType DalToModel(DalType dal);
+
+        protected List<ModelType> ListDalToListModel(List<DalType> dalList)
+        {
+            List<ModelType> listModel = new List<ModelType>();
+            for(int i=0;i<dalList.Count;i++)
+            {
+                listModel.Add(this.DalToModel(dalList[i]));
+            }
+
+            return listModel;
+        }
+
+        
+    }
+}
